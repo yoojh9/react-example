6f9530bd8559bcc7e9b4a8067b847f3e87705042
JeonghyunYoo <yoojh9@hancomm.com>
2018-03-16 16:55:42 +0900

    react-tutorial'

---
+++ react-tutorial/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App'; // js 확장자는 생략해도 됨
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App/>, rootElement);
